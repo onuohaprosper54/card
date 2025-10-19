6a925a8028b098380d74909778fed8b54d92d762
prospect54 <onuohaprosper54@gmail.com>
2025-10-19 04:49:00 -0400

    create oop version of cart

--- amazon.project/script/checkout.js
+++ amazon.project/script/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummry.js";
 import { renderPaymentSummy } from "./checkout/paymentSummary.js";
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 renderPaymentSummy();
