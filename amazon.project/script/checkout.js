1f0a9d8302c656d06e872260e77f8e6d2729a9a0
prospect54 <onuohaprosper54@gmail.com>
2025-10-19 05:44:35 -0400

    create class vision of cart

--- amazon.project/script/checkout.js
+++ amazon.project/script/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummry.js";
 import { renderPaymentSummy } from "./checkout/paymentSummary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummy();
